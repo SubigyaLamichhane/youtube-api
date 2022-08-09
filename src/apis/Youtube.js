5de5c42d549b77fa49c3327f808bed83acf8da8d
subigyalamichhane <subigyalamichhane@gmail.com>
2022-08-09 15:16:31 +0545

    key fix

--- src/apis/Youtube.js
+++ src/apis/Youtube.js
@@ -1,14 +1,13 @@
-
 import axios from 'axios';
+import keys from '../keys';
 
-
-const k = 'AIzaSyCxuPFjttzSPJC2JhDoqszbcZ4FW-W5gQs';
+const k = keys.GOOGLE_API_KEY; // Enter Google API Key
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: "snippet" ,
-        maxResults: 5,
-        key: k
-    }
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: k,
+  },
 });
